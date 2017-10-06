3ad709867fe11f627e06410ec4d0be1cecfa4029
qinyaomou <mouqinyao@gmail.com>
2017-10-06 08:16:10 -0400

    Update css.

--- app/routes/Home/Home.jsx
+++ app/routes/Home/Home.jsx
@@ -24,51 +24,53 @@ class Home extends Component {
 		      <div className="vehicle__name">Vehicle Name</div>
 		      <div className="home-hero"></div>
 		    </div>
-		    <div className="blade">          
-			    <div className="blade__heading" onClick={this.handleClick}>
-			      <img className="blade__icon" src="/app/images/icon_remoteService.svg"/>
-			      <div>
-			        SUBARU STARLINK&trade;
-			        <div className="blade__sub-heading">Remote Services</div>
-			        <p className="blade__heading-description">Lock, Unlock and Locate Your Subaru</p>
-			      </div>
-			      <div className="blade__heading-dots"></div>
+		    <div className="page__content">
+			    <div className="blade">          
+				    <div className="blade__heading" onClick={this.handleClick}>
+				      <img className="blade__icon" src="/app/images/icon_remoteService.svg"/>
+				      <div>
+				        SUBARU STARLINK&trade;
+				        <div className="blade__sub-heading">Remote Services</div>
+				        <p className="blade__heading-description">Lock, Unlock and Locate Your Subaru</p>
+				      </div>
+				      <div className="blade__heading-dots"></div>
+				    </div>
+				</div>
+				<div className="blade">         
+				    <div className="blade__heading">
+				      <img className="blade__icon" src="/app/images/icon_circleHealth.svg"/>
+				      <div>
+				        Vehicle Health
+				        <p className="blade__heading-description">5 alert(s) for you Subaru</p>
+				      </div>
+				      <div className="blade__heading-dots"></div>
+				    </div>
+				</div>
+				<div className="blade">
+			        <div className="blade__heading">
+			          <img className="blade__icon" src="/app/images/icon_circleMessage.png"/>
+			          <div>
+			            Messages
+			            <p className="blade__heading-description">You have 4 message(s)</p>
+			          </div>
+			          <div className="blade__heading-dots"></div>
+			        </div>
 			    </div>
-			</div>
-			<div className="blade">         
-			    <div className="blade__heading">
-			      <img className="blade__icon" src="/app/images/icon_circleHealth.svg"/>
-			      <div>
-			        Vehicle Health
-			        <p className="blade__heading-description">5 alert(s) for you Subaru</p>
-			      </div>
-			      <div className="blade__heading-dots"></div>
+			    <div className="blade">          
+			        <div className="blade__heading">
+			          <img className="blade__icon" src="/app/images/icon_driverAlert.svg"/>Driver Alerts Settings
+			        </div>
 			    </div>
-			</div>
-			<div className="blade">
-		        <div className="blade__heading">
-		          <img className="blade__icon" src="/app/images/icon_circleMessage.png"/>
-		          <div>
-		            Messages
-		            <p className="blade__heading-description">You have 4 message(s)</p>
-		          </div>
-		          <div className="blade__heading-dots"></div>
-		        </div>
-		    </div>
-		    <div className="blade">          
-		        <div className="blade__heading">
-		          <img className="blade__icon" src="/app/images/icon_driverAlert.svg"/>Driver Alerts Settings
-		        </div>
-		    </div>
-		    <div className="blade">         
-		        <div className="blade__heading">
-		          <img className="blade__icon" src="/app/images/icon_circleService.svg"/>
-		          <div>
-		            Services
-		            <p className="blade__heading-description">Manage your vehicle's services</p>
-		          </div>
-		          <div className="blade__heading-dots"></div>
-		        </div>
+			    <div className="blade">         
+			        <div className="blade__heading">
+			          <img className="blade__icon" src="/app/images/icon_circleService.svg"/>
+			          <div>
+			            Services
+			            <p className="blade__heading-description">Manage your vehicle's services</p>
+			          </div>
+			          <div className="blade__heading-dots"></div>
+			        </div>
+			    </div>
 		    </div>
 		  </div>
 		);
